361a498495d842114f2eb12df774423279ef050c
Ronnie Gross <rnngrss1495@gmail.co>
2019-02-14 14:47:31 -0500

    created functionality to add underlined divs depending on word length

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 // generate a word for the user to guess
-var wordBank = ["hello", "banana", "candy"];
+var wordBank = ["hey", "banana", "candy"];
 var randomNumber = Math.random();
 var randomWholeNumber = Math.floor(randomNumber * wordBank.length);
 var randomArrayWord = wordBank[randomWholeNumber];
 
 // generate a series of underlined divs that corresponds to the length of the randomly generated word
+// var letterBox = $( ".letterArea" ).append( "<div class='letterBox'><div class='underlinedLetterArea'></div></div>" );
+var letterBox = "<div class='letterBox'><div class='underlinedLetterArea'></div></div>";
 
-var letterBox = $( ".letterArea" ).append("<div class='letterBox'><div class='underlinedLetterArea'></div></div>");
-console.log(letterBox)
+// get the length of the randomly generated word
+
+var randomArrayWordLength = randomArrayWord.length;
+console.log(randomArrayWordLength)
+
+// create a series of boxes that corresponds to that length
+
+
+for (let i = 0; randomArrayWordLength > i; i++) {
+    $( ".letterArea" ).append(letterBox);
+}
 
