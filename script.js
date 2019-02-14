d07db7759357f10cd1df0a8e8e28ff705846a087
Ronnie Gross <rnngrss1495@gmail.co>
2019-02-14 14:03:02 -0500

    got randomly generated word from word bank working

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-var wordBank = ["hello", "banana", "candy"]
+// generate a word for the user to guess
+var wordBank = ["hello", "banana", "candy"];
+var randomNumber = Math.random();
+var randomWholeNumber = Math.floor(randomNumber * wordBank.length);
+var randomArrayWord = wordBank[randomWholeNumber]
+
+console.log(randomArrayWord);
