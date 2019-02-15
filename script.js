a5146c5aec4ae86465da60c3152fc73f54f81d40
Ronnie Gross <rnngrss1495@gmail.co>
2019-02-14 19:49:20 -0500

    got the letters to appear in the write spot and styled them

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 // generate a word for the user to guess
-var wordBank = ["hey", "banana", "candy"];
+var wordBank = ["banana"];
 var randomNumber = Math.random();
 var randomWholeNumber = Math.floor(randomNumber * wordBank.length);
 var randomArrayWord = wordBank[randomWholeNumber];
 
-// generate a series of underlined divs that corresponds to the length of the randomly generated word
-// var letterBox = $( ".letterArea" ).append( "<div class='letterBox'><div class='underlinedLetterArea'></div></div>" );
-var letterBox = "<div class='letterBox'><div class='underlinedLetterArea'></div></div>";
 
 // get the length of the randomly generated word
 var randomArrayWordLength = randomArrayWord.length;
 
 // create a series of boxes that corresponds to that length
 for (let i = 0; randomArrayWordLength > i; i++) {
-    $( ".letterArea" ).append(`<div id='box${i}' class='letterBox'><div class='underlinedLetterArea'></div></div>`);
+    var letterBox = `<div id='box${i}' class='letterBox'><div class='underlinedLetterArea'></div></div>`;
+    $( ".letterArea" ).append(letterBox);
 }
 
 // have the letter that was clicked on appear in a div 
@@ -22,6 +20,8 @@ let addLetter = () => {
 }
 
 console.log(randomArrayWord)
+
+
 
 $(".alphabetLetter").on( "click", function( event ) {
     // when user selects a letter, grey out the letter in the alphabet list
@@ -33,12 +33,10 @@ $(".alphabetLetter").on( "click", function( event ) {
         // need to target the exact location(s) in the word where the letter is located and add letter to that location
         for (let i = 0; i < randomArrayWordLength; i++) {
             if (randomArrayWord[i] == event.currentTarget.innerHTML) {
-                console.log("Found a broski here:" + i);
-                $("#box"+i).append(event.currentTarget.innerHTML);
-                // $( ".letterBox" )[includesLetterIndex].append(event.currentTarget.innerHTML);
+                var letterInBox =  `<p>${event.currentTarget.innerHTML}</p>`
+                $("#box"+i).append(letterInBox);
             }
-        }
-        
+        }   
     }
 });
 
